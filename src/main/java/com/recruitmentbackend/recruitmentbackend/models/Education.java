8d2936526ed6c8d2bce6e87da74cf1f22a4ba7c4
PatrikJMelander <patrikjmelander@gmail.com>
2022-01-18 10:28:14 +0100

    model Education done

--- src/main/java/com/recruitmentbackend/recruitmentbackend/models/Education.java
+++ src/main/java/com/recruitmentbackend/recruitmentbackend/models/Education.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -23,6 +24,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
+@Slf4j
 public class Education {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
