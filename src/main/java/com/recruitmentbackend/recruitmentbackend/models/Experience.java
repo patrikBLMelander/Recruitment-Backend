4a5807ca5f7561eedee972924389d63f48751238
PatrikJMelander <patrikjmelander@gmail.com>
2022-01-18 08:36:10 +0100

    model Experience done

---
+++ src/main/java/com/recruitmentbackend/recruitmentbackend/models/Experience.java
@@ -0,0 +1,38 @@
+package com.recruitmentbackend.recruitmentbackend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+/**
+ * Created by Patrik Melander
+ * Date: 2022-01-18
+ * Time: 08:34
+ * Project: Recruitment-Backend
+ * Copyright: MIT
+ */
+@Entity
+@Table(name = "experience")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Experience {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+    @Column(name = "description", length = 1000, nullable = false)
+    private String description;
+}
