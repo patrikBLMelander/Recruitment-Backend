91e1c9fa43affb2f3aca9466ca8d357a894f214a
PatrikJMelander <patrikjmelander@gmail.com>
2022-01-18 10:32:09 +0100

    creating url constants

---
+++ src/main/java/com/recruitmentbackend/recruitmentbackend/controller/AppConstants.java
@@ -0,0 +1,23 @@
+package com.recruitmentbackend.recruitmentbackend.controller;
+
+/**
+ * Created by Patrik Melander
+ * Date: 2022-01-18
+ * Time: 10:10
+ * Project: Recruitment-Backend
+ * Copyright: MIT
+ */
+public class AppConstants {
+    public static class API_MAPPING {
+        public static final String BASE_API = "/recruitment/v1";
+        public static final String PUBLIC = "/public";
+        public static final String CANDIDATES = "/candidates";
+        public static final String ADMIN = "/admin";
+        public static final String CREATE = "/create";
+        public static final String UPDATE = "/update";
+        public static final String EXPERIENCE = "/experience";
+        public static final String EDUCATION = "/education";
+        public static final String COMPETENCE = "/competence";
+    }
+}
+
