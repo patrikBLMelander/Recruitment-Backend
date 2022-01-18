9c48fbb5a770671fb7ffc75f3e613b103a5323d9
PatrikJMelander <patrikjmelander@gmail.com>
2022-01-18 08:39:02 +0100

    model Competence done

---
+++ src/main/java/com/recruitmentbackend/recruitmentbackend/models/Competence.java
@@ -0,0 +1,33 @@
+package com.recruitmentbackend.recruitmentbackend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+/**
+ * Created by Patrik Melander
+ * Date: 2022-01-18
+ * Time: 08:37
+ * Project: Recruitment-Backend
+ * Copyright: MIT
+ */
+@Entity
+@Table(name = "competence")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Competence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "value", nullable = false)
+    private Integer value;
+}
